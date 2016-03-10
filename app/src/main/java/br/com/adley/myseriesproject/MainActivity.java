aca3ed2c457b937be9e733e051aeeda53eabc9a8
Adley Damaceno <adleydamaceno@gmail.com>
2016-03-10 16:55:13 -0300

    Remove commented block

--- app/src/main/java/br/com/adley/myseriesproject/MainActivity.java
+++ app/src/main/java/br/com/adley/myseriesproject/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView idSeriesList;
     private Button idSearchButton;
     private EditText idInputNameSerie;
-    private static final String APIURL="http://api.tvmaze.com";
+    private static final String APIURL = "http://api.tvmaze.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,100 +26,17 @@ public class MainActivity extends AppCompatActivity {
         idInputNameSerie = (EditText) findViewById(R.id.idInputNameSerie);
 
         idSearchButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (idInputNameSerie.getText().toString().isEmpty()) {
-                        Toast.makeText(MainActivity.this, "ERRO: Insira um nome", Toast.LENGTH_SHORT).show();
-                    }else {
-                        //String url = APIURL + "/search/shows?q=" + idInputNameSerie.getText().toString();
-                        Toast.makeText(MainActivity.this, "Buscando...", Toast.LENGTH_LONG).show();
-                        GetTVShowJsonData jsonData = new GetTVShowJsonData(idInputNameSerie.getText().toString());
-                        jsonData.execute();
-                    }
-                }
-            });
-    }
-    /*
-    public class JSONTask extends AsyncTask<String,String,List<String> > {
-        @Override
-        protected List<String> doInBackground(String... params) {
-
-            BufferedReader reader = null;
-            HttpURLConnection connection = null;
-            try {
-                URL reqURL = new URL(params[0]);
-                connection = (HttpURLConnection) (reqURL.openConnection());
-                connection.connect();
-                //connection.setRequestMethod("GET");
-                InputStream in = new BufferedInputStream(connection.getInputStream());
-
-                reader = new BufferedReader(new InputStreamReader(in));
-                String line;
-                StringBuilder result = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-                String finalJson = result.toString();
-                List<String> listNames = new ArrayList<String>();
-                JSONArray jsonArray = new JSONArray(finalJson);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonobject = jsonArray.getJSONObject(i);
-                    JSONObject oneShowJsonObject = new JSONObject(jsonobject.toString());
-                    JSONObject show = oneShowJsonObject.getJSONObject("show");
-                    listNames.add(show.getString("name"));
-                }
-
-                return listNames;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                Toast.makeText(MainActivity.this, "MalformedUrlException error", Toast.LENGTH_LONG).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Toast.makeText(MainActivity.this, "IoException error", Toast.LENGTH_LONG).show();
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(MainActivity.this, "JSONException error", Toast.LENGTH_LONG).show();
-            } finally {
-                try{
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-                    if (reader != null) {
-                        reader.close();
-                    }
-                }catch (IOException e){
-                    Toast.makeText(MainActivity.this, "Finally IOException error", Toast.LENGTH_LONG).show();
-                }
-                catch (Exception e){
-                    Toast.makeText(MainActivity.this, "Finally Exception error", Toast.LENGTH_LONG).show();
+            @Override
+            public void onClick(View v) {
+                if (idInputNameSerie.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "ERRO: Insira um nome", Toast.LENGTH_SHORT).show();
+                } else {
+                    //String url = APIURL + "/search/shows?q=" + idInputNameSerie.getText().toString();
+                    Toast.makeText(MainActivity.this, "Buscando...", Toast.LENGTH_LONG).show();
+                    GetTVShowJsonData jsonData = new GetTVShowJsonData(idInputNameSerie.getText().toString());
+                    jsonData.execute();
                 }
             }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(final List<String> listNames) {
-            super.onPostExecute(listNames);
-            if(!listNames.isEmpty()) {
-                idSeriesList = (ListView) findViewById(R.id.idSeriesList);
-                ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
-                        getApplicationContext(),
-                        android.R.layout.simple_list_item_2,
-                        android.R.id.text2,
-                        listNames
-                );
-                idSeriesList.setAdapter(adaptador);
-                idSeriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Toast.makeText(MainActivity.this, listNames.get(position).toString() , Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }else{
-                Toast.makeText(MainActivity.this, "Ocorreu um erro no onPostExecute", Toast.LENGTH_LONG).show();
-            }
-        }
+        });
     }
-    */
 }
