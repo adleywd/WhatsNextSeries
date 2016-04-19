f9c5b7064d846f2a0ad5ffeeb142412848ac164f
Adley Damaceno <adleydamaceno@gmail.com>
2016-04-19 11:12:33 -0300

    Working on the new API structure.This version do not compile

--- app/src/main/java/br/com/adley/myseriesproject/themoviedb/GetTVShowJsonData.java
+++ app/src/main/java/br/com/adley/myseriesproject/themoviedb/GetTVShowJsonData.java
@@ -12,9 +12,11 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import br.com.adley.myseriesproject.library.enums.DownloadStatus;
 import br.com.adley.myseriesproject.library.GetRawData;
+import br.com.adley.myseriesproject.models.TVShow;
 import br.com.adley.myseriesproject.tvmaze.TVMazeTVShowObject;
 import br.com.adley.myseriesproject.library.Utils;
 
@@ -25,27 +27,27 @@ import br.com.adley.myseriesproject.library.Utils;
 public class GetTVShowJsonData extends GetRawData{
 
     private String LOG_TAG = GetTVShowJsonData.class.getSimpleName();
-    private List<TVMazeTVShowObject> tvshows;
+    private List<TVShow> tvshows;
     private Uri destinationUri;
     private Context context;
 
-    public GetTVShowJsonData(String showToSearch, Context context) {
+    public GetTVShowJsonData(String showName, Context context) {
         super(null);
         tvshows = new ArrayList<>();
-        createAndUpdateUri(showToSearch);
+        createAndUpdateUri(showName);
         this.context = context;
     }
 
     public void execute(){
         super.setRawUrl(destinationUri.toString());
         DownloadJsonData downloadJsonData = new DownloadJsonData();
-        Log.v(LOG_TAG, "Built Uri = "+ destinationUri.toString());
+        //Log.v(LOG_TAG, "Built Uri = "+ destinationUri.toString());
         downloadJsonData.execute(destinationUri.toString());
     }
     public boolean createAndUpdateUri(String showName) {
         final String BASE_URL_API_SEARCH = "http://api.themoviedb.org/3/search/tv";
-        final String API_KEY_THEMOVIEDBKEY = Utils.getApiKey(context);
-        final String API_KEY = "api_key_value";
+        final String API_KEY_THEMOVIEDBKEY = "api_key";
+        final String API_KEY = Utils.getApiKey(context);
         final String LANGUAGE_THEMOVIEDBKEY = "language";
         final String LANGUAGE = "pt-br";
 
@@ -60,7 +62,7 @@ public class GetTVShowJsonData extends GetRawData{
         return destinationUri != null;
     }
 
-    public  List<TVMazeTVShowObject> getTVShows(){
+    public  List<TVShow> getTVShows(){
         return tvshows;
     }
 
@@ -69,30 +71,55 @@ public class GetTVShowJsonData extends GetRawData{
             Log.e(LOG_TAG, "Error download raw file");
             return;
         }
-
+        final String PAGE_SEARCH_TVSHOW = "page";
+        final String RESULTS_SEARCH_TVSHOW = "results";
+        final String TOTAL_RESULTS_SEARCH_TVSHOW = "total_results";
+        final String POSTER_PATH_TVSHOW = "POSTER_PATH";
+        final String POPULARITY_TVSHOW = "popu";
         final String ID_TVSHOW = "id";
-        final String URL_TVSHOW = "url";
-        final String NAME_TVSHOW = "name";
-        final String TYPE_TVSHOW = "type";
-        final String LANGUAGE_TVSHOW = "language";
-        final String STATUS_TVSHOW = "status";
-        final String SUMMARY_TVSHOW = "summary";
-        final String IMAGE_TVSHOW = "image";
-        final String IMAGE_MEDIUM_TVSHOW = "medium";
-        final String IMAGE_ORIGINAL_TVSHOW = "original";
-        final String LINKS_TVSHOW = "_links";
-        final String PREVIOUS_EPISODE_TVSHOW = "previousepisode";
-        final String NEXT_EPISODE_TVSHOW = "nextepisode";
-        final String HREF_EPISODES_TVSHOW = "href";
+        final String BACKDROP_PATH_TVSHOW = "backdrop_path";
+        final String VOTE_AVERAGE_TVSHOW = "vote_average";
+        final String OVERVIEW_TVSHOW = "overview";
+        final String FIRST_AIR_DATE_TVSHOW = "first_air_date";
+        final String ORIGINAL_LANGUAGE_TVSHOW = "original_language";
+        final String VOTE_COUNT_TVSHOW = "vote_count";
+        final String NAME_TVSHOW = "Arqueiro";
+        final String ORIGINAL_NAME_TVSHOW = "Arrow";
+
+        // TODO: Lists
+        //final String ORIGIN_COUNTRY_TVSHOW = "origin_country";
+        //final String GENRES_IDS = "genre_ids";
+
+      /*  EXAMPLE JSON
+        {
+        "page": 1,
+        "results": [
+            {
+            "poster_path": "/thisPICTURElink312332.jpg",
+            "popularity": 10.980402,
+            "id": 1412,
+            "backdrop_path": "/PICTURElink312332.jpg",
+            "vote_average": 6.8,
+            "overview": " some overview here.",
+            "first_air_date": "2012-10-10",
+            "original_language": "en",
+            "vote_count": 287,
+            "name": "Translated Name",
+            "original_name": "SomeName"
+            },
+            {}
+        }
+    */
 
         try {
             // Navigate and parse the JSON Data
-            JSONArray jsonArray = new JSONArray(getData());
-            if (jsonArray.length() == 0){
+            JSONObject jsonObject = new JSONObject(getData());
+            jsonObject.getString(RESULTS_SEARCH_TVSHOW);
+            if (jsonObject.length() == 0){
                 Toast.makeText(context, "Nenhuma série encontrada", Toast.LENGTH_SHORT).show();
                 return;
             }
-            for (int i = 0; i < jsonArray.length(); i++) {
+            for (int i = 0; i < jsonObject.length(); i++) {
                 JSONObject jsonobject = jsonArray.getJSONObject(i);
                 JSONObject oneShowJsonObject = new JSONObject(jsonobject.toString());
                 JSONObject showJSonObject = oneShowJsonObject.getJSONObject("show");
@@ -147,10 +174,12 @@ public class GetTVShowJsonData extends GetRawData{
                 }
 
                 // Create TVMazeTVShowObject Object and add to the List of Shows
-                TVMazeTVShowObject TVMazeTvShowObjectObject = new TVMazeTVShowObject(id, url, name, type, language, status, imageMedium, imageOriginal, summary, previousEpisode, nextEpisode);
-                this.tvshows.add(TVMazeTvShowObjectObject);
+                TVShow tvShow = new TVShow();//id, url, name, type, language, status, imageMedium, imageOriginal, summary, previousEpisode, nextEpisode);
+                this.tvshows.add(tvShow);
             }
-            for (TVMazeTVShowObject singleShow: tvshows){
+
+            /*  --- LOG EACH SHOW FOUND -- */
+            for (TVShow singleShow: tvshows){
                 Log.v(LOG_TAG, singleShow.toString());
             }
 
