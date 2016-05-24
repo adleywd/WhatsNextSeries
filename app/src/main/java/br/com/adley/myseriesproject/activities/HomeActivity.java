59141c734ccff8ab2dc9557a7eb1d25f486e4278
Adley Damaceno <adleydamaceno@gmail.com>
2016-05-24 14:54:55 -0300

    Add Ads AdMob

--- app/src/main/java/br/com/adley/myseriesproject/activities/HomeActivity.java
+++ app/src/main/java/br/com/adley/myseriesproject/activities/HomeActivity.java
@@ -15,6 +15,10 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,6 +47,7 @@ public class HomeActivity extends BaseActivity {
     private View mProgressBarHomeLayout;
     private ProgressBar mProgressBarHome;
     private int mProgressBarCount = 0;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +63,25 @@ public class HomeActivity extends BaseActivity {
         mProgressBarHome = (ProgressBar) findViewById(R.id.shared_progressbar_home);
         executeHomeContent(false);
 
+        //Ad Config
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, getString(R.string.banner_ad_unit_id));
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        mAdView = (AdView) findViewById(R.id.ad_view);
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice(getString(R.string.device_id_test1))
+                .build();
+
+        // Start loading the ad in the background.
+        mAdView.setVisibility(View.VISIBLE);
+        mAdView.loadAd(adRequest);
 
     }
 
@@ -282,6 +306,34 @@ public class HomeActivity extends BaseActivity {
             startActivity(new Intent(this, AppPreferences.class));
         }
         return true;
+    }
+
+    // AdConfig
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
     }
 }
 
