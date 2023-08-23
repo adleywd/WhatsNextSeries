ff003744a3b01a824322e90edbb890f04143618d
Adley Damaceno <adleydamaceno@gmail.com>
2023-08-23 22:57:05 +0100

    Added working view tab

---
+++ src/WhatsNextSeries/Components/TvShowCard.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace WhatsNextSeries.Components;
+
+public class TvShowCard : TemplatedControl
+{
+    public static readonly StyledProperty<string> TvShowNameProperty = 
+        AvaloniaProperty.Register<TvShowCard, string>(nameof(TvShowName), "Tv Show Name");
+   
+    public string TvShowName
+    {
+        get => GetValue(TvShowNameProperty);
+        set => SetValue(TvShowNameProperty, value);
+    }
+    
+}
