3cfc0a7433bf9508ebdc8715eba7a1c79dfd2276
Adley Damaceno <adleydamaceno@gmail.com>
2023-08-26 15:47:34 +0100

    Removed auto generated code

---
+++ src/WhatsNextSeries.Services/TheMovieDbApiKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsNextSeries.Services
+{
+    public static class TheMovieDbApiKey
+    {
+        public const string ApiKey = "YOUR_API_KEY";
+    }
+}
