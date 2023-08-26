e12e45121aad21462aba943a73acc79d7f6c10f5
Adley Damaceno <adleydamaceno@gmail.com>
2023-08-26 01:27:50 +0100

    Improve tabs

--- src/WhatsNextSeries.Services/DummyMovieDbService.cs
+++ src/WhatsNextSeries.Services/DummyMovieDbService.cs
@@ -8,51 +8,35 @@ public class DummyMovieDbService : IMovieDbService
     {
         return Task.FromResult(new List<TvShow>
         {
-            new ()
-            {
-                BackDropSize = "w1280",
-                FirstAirDate = "2021-09-22",
-                Id = 1416,
-                Name = "Grey's Anatomy",
-                Overview = "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
-                PosterSize = "w342",
-                VoteAverage = 8.2,
-                Popularity = 3142.436,
-                VoteCount = 2749,
-                PosterPath = "",
-                GenreIds = new List<int>(),
-                BackdropPath = "",
-            },
-            new ()
-            {
-                BackDropSize = "w1280",
-                FirstAirDate = "2021-09-22",
-                Id = 1416,
-                Name = "Grey's Anatomy",
-                Overview = "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
-                PosterSize = "w342",
-                VoteAverage = 8.2,
-                Popularity = 3142.436,
-                VoteCount = 2749,
-                PosterPath = "",
-                GenreIds = new List<int>(),
-                BackdropPath = "",
-            },
-            new ()
-            {
-                BackDropSize = "w1280",
-                FirstAirDate = "2021-09-22",
-                Id = 1416,
-                Name = "Grey's Anatomy",
-                Overview = "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
-                PosterSize = "w342",
-                VoteAverage = 8.2,
-                Popularity = 3142.436,
-                VoteCount = 2749,
-                PosterPath = "",
-                GenreIds = new List<int>(),
-                BackdropPath = "",
-            },
+            CreateShow("Grey's Anatomy"),
+            CreateShow("The Walking Dead"),
+            CreateShow("The Good Doctor"),
+            CreateShow("The Simpsons"),
+            CreateShow("The Flash"),
+            CreateShow("The Big Bang Theory"),
+            CreateShow("Family Guy"),
+            CreateShow("The Blacklist"),
+            CreateShow("The Rookie"),
+            CreateShow("The Mandalorian"),
+            CreateShow("The Good Doctor"),
+            CreateShow("The Simpsons"),
+            CreateShow("The Flash"),
         });
     }
+
+    private static TvShow CreateShow(string name) => new TvShow
+    {
+        BackDropSize = "w1280",
+        FirstAirDate = "2021-09-22",
+        Id = 1416,
+        Name = name,
+        Overview = "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
+        PosterSize = "w342",
+        VoteAverage = 8.2,
+        Popularity = 3142.436,
+        VoteCount = 2749,
+        PosterPath = "",
+        GenreIds = new List<int>(),
+        BackdropPath = "",
+    };
 }
