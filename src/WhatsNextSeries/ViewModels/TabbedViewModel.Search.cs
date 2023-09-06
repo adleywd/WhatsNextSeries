209219417c6e510820e15b3425bc20422b8951ac
Adley Damaceno <Adley.Damaceno@vision-box.com>
2023-09-06 17:55:15 +0100

    Added placeholder for search view

---
+++ src/WhatsNextSeries/ViewModels/TabbedViewModel.Search.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WhatsNextSeries.ViewModels;
+
+public partial class TabbedViewModel
+{
+    [ObservableProperty]
+    private string _searchText = string.Empty;
+    
+    public ObservableCollection<string> SearchResults { get; } = new();
+    
+    [RelayCommand]
+    private Task SearchAsync()
+    {
+        SearchResults.Add("test");
+        return Task.CompletedTask;
+    }
+
+}
---
+++ src/WhatsNextSeries/Views/SearchView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WhatsNextSeries.Views;
+
+public partial class SearchView : UserControl
+{
+    public SearchView()
+    {
+        InitializeComponent();
+    }
+
+    private void ScrollViewer_OnScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+    }
+}
